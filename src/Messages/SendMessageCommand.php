ab3796bbe7448bfe85c8c564ff3e90b93a515905
Santi <santiripper@gmail.com>
2020-04-08 14:02:58 +0200

    Adds a `data` payload field for Messages

--- src/Messages/SendMessageCommand.php
+++ src/Messages/SendMessageCommand.php
@@ -11,18 +11,21 @@ class SendMessageCommand
     public $conversation;
     public $type;
     public $participant;
+    public $data;
 
     /**
      * @param Conversation $conversation The conversation
      * @param string       $body         The message body
      * @param Model        $sender       The sender identifier
      * @param string       $type         The message type
+     * @param array|null   $data         The message data payload 
      */
-    public function __construct(Conversation $conversation, $body, Model $sender, $type = 'text')
+    public function __construct(Conversation $conversation, $body, Model $sender, $type = 'text', $data = null)
     {
         $this->conversation = $conversation;
         $this->body = $body;
         $this->type = $type;
         $this->participant = $this->conversation->participantFromSender($sender);
+        $this->data = $data;
     }
 }
